e6f8b0c5a207b64f621ad25dcb94ba24254d1dc5
Fyodor <fyezhlo@gmail.com>
2023-09-24 20:16:29 +0300

    server n handlers

--- ffcrm.go
+++ ffcrm.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+const url = "localhost:8080"
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Homepage\n"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Snippet page\n"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("New snippet form\n"))
+}
+
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Printf("starting web server on %s", url)
+	err := http.ListenAndServe(url, mux)
+	log.Fatal(err)
+}
