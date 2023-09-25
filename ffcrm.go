f2192fa312562cd834fb72117adbbea2896c7650
Fyodor <fyezhlo@gmail.com>
2023-09-25 10:18:40 +0300

    err handle n url params

--- ffcrm.go
+++ ffcrm.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 const url = "localhost:8080"
@@ -16,13 +18,22 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Snippet page\n"))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Snippet page with id=%d \n", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "405 Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("New snippet form\n"))
 }
-
 
 func main() {
 	mux := http.NewServeMux()
